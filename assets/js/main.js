447eb3bd10c0da847a873d02f4b20f77cac9b452
Your Name <you@example.com>
2023-12-14 22:39:45 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,28 @@ $(document).ready(function(){
     $('.popup-modal #search form input').on('keyup',function(e){
         $('.popup-modal #search .result').toggleClass('show',$(this).val().length > 0)
     })
+
+   
 })
+
+// XỬ LÝ POPUP SEARCH    
+// ĐÓNG
+function closePopup(e){
+   
+      $(document).ready(function(){
+
+          $('.popup-fixed .popup-close').click(function(){
+            console.log($(this).parent().attr('id'))
+            $('#'+$(this).parent().attr('id')).removeClass('show')
+          })
+      })
+   
+}
+// MỞ
+function openPopup(e){
+      $(document).ready(function(){
+           let popup =  $(e).attr('data-popup')
+           $('#'+popup).addClass('show')
+      })
+   
+}
